caa711e17341814f5de18e4d73b322a86e07367b
tourn <latzer.daniel@gmail.com>
2016-04-27 17:41:49 +0200

    LET THERE BE LIGHT

---
+++ README.md
@@ -0,0 +1,10 @@
+A bot that supports inline queries for dota 2 responses.
+
+Heavily based on https://github.com/Jonarzz/DotaResponsesRedditBot
+
+Setup
+=====
+Build a responses.db and heroes.json from https://github.com/Jonarzz/DotaResponsesRedditBot and put it in the project root
+(for the heroes.json, uncomment the last lines from dota_wiki_parser.py and run it)
+(for the responses.db, uncomment create_responses_database from dota_responses_database.py and run that)
+
---
+++ bot.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+This Bot uses the Updater class to handle the bot.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic inline bot example. Applies different text transformations.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+from uuid import uuid4
+
+import re
+import os
+
+from telegram import InlineQueryResultArticle, ParseMode, \
+    InputTextMessageContent
+from telegram import InlineQueryResultAudio
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import logging
+import sqlite3
+
+import dota_wiki_parser as parser
+import properties
+
+SCRIPT_DIR = os.path.dirname(__file__)
+heroes_dict = parser.dictionary_from_file("heroes.json")
+
+#RESPONSES_DB_CONN = sqlite3.connect(os.path.join(SCRIPT_DIR, 'responses.db'))
+#RESPONSES_DB_CURSOR = RESPONSES_DB_CONN.cursor()
+
+# Enable logging
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi!')
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Help!')
+
+
+def escape_markdown(text):
+    """Helper function to escape telegram markup symbols"""
+    escape_chars = '\*_`\['
+    return re.sub(r'([%s])' % escape_chars, r'\\\1', text)
+
+
+def inlinequery(bot, update):
+    query = update.inline_query.query
+    results = list()
+    #logger.info("query: " + query)
+    if (query == ""):
+        return
+
+    RESPONSES_DB_CONN = sqlite3.connect(os.path.join(SCRIPT_DIR, 'responses.db'))
+    RESPONSES_DB_CURSOR = RESPONSES_DB_CONN.cursor()
+    for row in RESPONSES_DB_CURSOR.execute("SELECT response, link FROM responses WHERE response LIKE ? LIMIT 50", ["%" + query + "%"]):
+        short_hero_name = parser.short_hero_name_from_url(row[1])
+        hero_name = heroes_dict[short_hero_name]
+        #logger.info(row)
+        results.append(InlineQueryResultAudio(
+                id=uuid4(),
+                title=row[0],
+                audio_url=row[1],
+                performer=hero_name))
+
+
+    bot.answerInlineQuery(update.inline_query.id, results=results)
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(properties.TELEGRAM_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.addHandler(CommandHandler("start", start))
+    dp.addHandler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.addHandler(InlineQueryHandler(inlinequery))
+
+    # log all errors
+    dp.addErrorHandler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
